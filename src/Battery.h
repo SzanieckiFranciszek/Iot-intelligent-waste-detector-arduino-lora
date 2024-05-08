b597f86fe5e04ccf2d8a1e98b2a72cd5d7e9267e
Franciszek <franekszaniecki@gmail.com>
2024-05-08 23:04:10 +0200

    Added mesuring battery voltage messuring and change analog data into real input batttery voltage. Update battery status ranges.

--- src/Battery.h
+++ src/Battery.h
@@ -14,42 +14,53 @@ public:
         pinMode(analogMeasurePin, INPUT);
     }
 
-     float measureBatteryStatusOnAnalogInput() {
-        Serial.println("Measuring battery voltage in progress:");
+     float measureBatteryVoltageOnAnalogInput() {
+        Serial.print("Measuring battery voltage in progress:");
 
         readAnalogValue = analogRead(analogMeasurePin);
 
-        return readAnalogValue;
+        mesuredVoltage = readAnalogValue * (3.3/1024.0);
+        realInputVoltage =  mesuredVoltage * ((resistorOne + resistorTwo) / resistorTwo); //Real battery voltage without voltage divider
+        
+        return realInputVoltage;
     }
 
-     void printBatteryVoltage(float readValue) {
+     void printBatteryVoltage(float voltage) {
         Serial.println("Measured battery voltage: ");
-        voltage = readValue * (3.3/1024.0);
-        Serial.print(voltage);
+        Serial.println(voltage);
+        Serial.println(" V");
     }
 
     int calculateBatteryStatusInPercentage() {
-    float analogReadValue = measureBatteryStatusOnAnalogInput();
-    printBatteryVoltage(analogReadValue);
-    if (voltage >= 1024) {
-        return 100; // 100% Battery Status
-    } else if (voltage >= 972) {
-        return 75; // 75% Battery Status
-    } else if (voltage >= 921) {
-        return 50; // 50% Battery Status
-    } else if (voltage >= 901) {
-        return 25; // 25% Battery Status
-    } else if (voltage >= 870) {
-        return 5; // 5% Battery Status
-    } else {
-        return 0; // Battery Status
+    float batteryVoltage = measureBatteryVoltageOnAnalogInput();
+
+    printBatteryVoltage(batteryVoltage);
+
+    if (mesuredVoltage >= 4.15) { // Max battery voltage = 4.2 V
+        batteryPercentage = 100; // 100% Battery Status
+    } else if (mesuredVoltage >= 4.00 || mesuredVoltage < 4.15) {
+        batteryPercentage = 75; // 75% Battery Status
+    } else if (mesuredVoltage >= 3.80 || mesuredVoltage < 4.00) {
+        batteryPercentage = 50; // 50% Battery Status
+    } else if (mesuredVoltage >= 3.70 || mesuredVoltage < 3.80) {
+        batteryPercentage = 25; // 25% Battery Status
+    } else if (mesuredVoltage >= 3.60 || mesuredVoltage < 3.70) {
+        batteryPercentage = 5; // 5% Battery Status
     }
+    Serial.print("Battery status: ");
+    Serial.println(batteryPercentage);
+    Serial.println(" %");
+    return batteryPercentage;
 }
 
 private:
     float analogMeasurePin;
     int readAnalogValue = 0;
-    float voltage = 0;
+    float mesuredVoltage = 0;
+    float realInputVoltage = 0;
+    float resistorOne = 10000;
+    float resistorTwo = 22000;
+    int batteryPercentage = 0;
 };
 
 #endif // BATTERY_H
