122e12a5cf1c6c7143d0760c0c4816678a153dc8
Franciszek <franekszaniecki@gmail.com>
2024-06-22 15:14:12 +0200

    Added/ttn javascript decoder file.

--- ttn_decoder.js
+++ ttn_decoder.js
@@ -1,36 +1,36 @@
 function Decoder(bytes, port) {
-  var decoded = {};
-
-  var msg = String.fromCharCode.apply(null, bytes);
-	var i = 1; 
-	var deviceId = '';
-	var dumpsterFilingPercentage = '';
-	var batteryStatus = '';
-
-	while(i < msg.length && msg[i] != "P")
-	{
-		deviceId += msg[i];
-	  i++;
-	}
-  decoded.deviceId = deviceId;
-
-  i++;
+	var decoded = {};
+  
+	var msg = String.fromCharCode.apply(null, bytes);
+	  var i = 1; 
+	  var deviceId = '';
+	  var dumpsterFilingPercentage = '';
+	  var batteryStatus = '';
+  
+	  while(i < msg.length && msg[i] != "P")
+	  {
+		  deviceId += msg[i];
+		i++;
+	  }
+	decoded.deviceId = deviceId;
+  
+	i++;
+	  
+	  while(i < msg.length && msg[i] != "B")
+	  {
+		  dumpsterFilingPercentage += msg[i];
+		i++;
+	  }
+	decoded.dumpsterFilingPercentage = dumpsterFilingPercentage;
 	
-	while(i < msg.length && msg[i] != "B")
-	{
-		dumpsterFilingPercentage += msg[i];
-	  i++;
-	}
-  decoded.dumpsterFilingPercentage = dumpsterFilingPercentage;
-  
-  i++;
-  
-  while(i < msg.length)
-	{
-		batteryStatus += msg[i];
-	  i++;
-	}
-  decoded.batteryStatus = batteryStatus;
-  
-  return decoded;
-}
+	i++;
+	
+	while(i < msg.length)
+	  {
+		  batteryStatus += msg[i];
+		i++;
+	  }
+	decoded.batteryStatus = batteryStatus;
+	
+	return decoded;
+  }
